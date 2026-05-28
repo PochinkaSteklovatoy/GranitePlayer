5e98a5efaf89ce6d9ea641f7b1ce2104c3d73948
PochinkaSteklovatoy <amirah.ufa@gmail.com>
2026-05-29 00:03:39 +0500

    UI screens half done + data prototype
    Got to finish screens and then go to data structure? SQLite usage and etc.

---
+++ GranitePlayer/app/src/main/java/com/example/graniteplayer/TrackData.java
@@ -0,0 +1,13 @@
+package com.example.graniteplayer;
+
+public class TrackData {
+    private int id;
+    private String name;
+    private String author;
+    private String album;
+    private int time_min;
+    private int time_sec;
+
+    private String track_path;
+
+}
